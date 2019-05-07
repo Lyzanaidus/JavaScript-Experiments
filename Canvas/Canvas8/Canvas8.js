999bbecce5a03f2529e5403104a8a0d6996595c2
Lyzanaidus <wcrecent@gmail.com>
2019-05-07 13:08:19 +0545

    Fire

--- Canvas/Canvas8/Canvas8.js
+++ Canvas/Canvas8/Canvas8.js
@@ -14,6 +14,19 @@ canvas.width = maxCanvasWidth - 6;
 canvas.height = maxCanvasHeight - 6;
 
 //	Canvas related functions
+function dot() 
+{
+	ctx.fillStyle = 'white';
+	ctx.fillRect(0,0,3,3);
+
+}
+//	Function to display Bg
+function displayBg() 
+{
+	var bg = new Image();
+	bg.src = "Images/background.jpg";
+	ctx.drawImage(bg,minCanvasWidth,minCanvasHeight,maxCanvasWidth,maxCanvasHeight);
+}
 
 //	Function to clear entire canvas
 function clearCanvas() 
@@ -24,7 +37,7 @@ function clearCanvas()
 //	Avatar Class
 class Avatar
 {
-	constructor(xPos,yPos,imgSrc,imgWidth,imgHeight,rows,cols)
+	constructor(xPos,yPos,imgSrc,imgWidth,imgHeight,rows,cols,hp)
 	{
 		this.xPos = xPos;
 		this.yPos = yPos;
@@ -62,10 +75,13 @@ class Avatar
 		this.upKey = 38;
 		this.downKey = 40;
 
+		this.hp = hp;
+
 		this.sheetCurrentRow = 0;
 		this.sheetCurrentCol = 0;
 		this.nextSpriteCounter = 0;
-
+		this.ammoArr = [];
+		
 		document.addEventListener(
 								'keydown',
 								event =>
@@ -73,6 +89,7 @@ class Avatar
 									//console.log('[Class Avatar]\n','Avatar Input');
 									
 									//console.log('event.keyCode : ',event.keyCode);
+									
 									if (event.keyCode === this.upKey) 
 									{
 										//console.log('up pressed.');
@@ -100,7 +117,7 @@ class Avatar
 										this.nextMove[2] = false;
 										this.isFacingLeft = false;
 									}
-									console.log(this.nextMove);
+									//console.log(this.nextMove);
 									this.updateAnimation();
 									this.updatePos();
 								}
@@ -138,6 +155,18 @@ class Avatar
 								}
 								);
 
+		var AvatarObject = this;
+		var can = document.getElementById('canvas-1')
+		can.onclick = function (event) 
+								{
+									
+									//console.log('clicked at ',event.clientX,event.clientY);
+									AvatarObject.targetXPos = event.clientX;
+									AvatarObject.targetYPos = event.clientY;
+									AvatarObject.fire(AvatarObject.targetXPos,AvatarObject.targetYPos);
+									
+								}
+
 	}
 
 	animate(sheetCurrentRow,sheetCurrentCol) 
@@ -166,7 +195,7 @@ class Avatar
 
 		ctx.drawImage(this.sheet,this.sheetWinXPos,this.sheetWinYPos,this.sheetWinWidth,this.sheetWinHeight,this.xPos,this.yPos,this.width,this.height);
 		/*
-		console.log('sheet : ',this.sheet,'\nsheetWinXPos : ',this.sheetWinXPos,'\nsheetWinYPos : ',this.sheetWinYPos
+		//console.log('sheet : ',this.sheet,'\nsheetWinXPos : ',this.sheetWinXPos,'\nsheetWinYPos : ',this.sheetWinYPos
 					,'\nsheetWinWidth : ',this.sheetWinWidth,'\nsheetWinHeight : ',this.sheetWinHeight
 					,'\nXPos : ',this.xPos,'\nYPos : ',this.yPos,'\nwidth : ',this.width,'\nheight : ',this.height);
 		*/
@@ -254,37 +283,240 @@ class Avatar
 		this.move(this.dx,this.dy);
 	}
 
+	fire(tXPos,tYPos) 
+	{
+		//console.log('Function : fire()');
+		
+		var img = new Image();
+		img.src = "Images/ball.png";
+		img.width = 655;
+		img.height = 330;
+		var noOfRows = 4;
+		var noOfCols = 8;
+		var targetXPos = tXPos;
+		var targetYPos = tYPos;
+
+		if (!this.isFacingLeft) 
+		{
+			if (targetXPos >= this.xPos + this.width) 
+			{
+				this.bulletNo++;
+				this.ammoArr.push(new Ball(this.xPos,this.yPos,img.src,img.width,img.height,noOfRows,noOfCols,targetXPos,targetYPos,this.isFacingLeft));
+			}
+		}
+		else 
+		{
+			if (targetXPos < this.xPos) 
+			{
+				this.bulletNo++;
+				this.ammoArr.push(new Ball(this.xPos,this.yPos,img.src,img.width,img.height,noOfRows,noOfCols,targetXPos,targetYPos,this.isFacingLeft));
+			}
+		}
+		//console.log('ammoArr[',this.bulletNo,'] : ',this.ammoArr[this.bulletNo]);
+	}
+
 }
 
 /*function initPlayer1() 
 {*/
-	let initXPos = 20;
-	let initYPos = 50;
+	let initXPos = 0;
+	let initYPos = 0;
 	let img = new Image();
 	img.src = "Images/john.png";
 	img.width = 640;
 	img.height = 480;
 	let noOfRows = 6;
 	let noOfCols = 8;
+	let hp = 100;
 
-	// console.log(imga.width);
-	var player1 = new Avatar(initXPos,initYPos,img.src,img.width,img.height,noOfRows,noOfCols);
+	var player1 = new Avatar(initXPos,initYPos,img.src,img.width,img.height,noOfRows,noOfCols,hp);
 
 //}
+
+//	Ball Class
+class Ball
+{
+	constructor(xPos,yPos,imgSrc,imgWidth,imgHeight,rows,cols,targetXPos,targetYPos,isFacingLeft)
+	{
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.noOfRows = rows;
+		this.noOfCols = cols;
+
+		this.sheet = new Image();
+		this.sheet.src = imgSrc;
+		this.sheetWidth = imgWidth;
+		this.sheetHeight = imgHeight;
+		//console.log('[Class Ball]\n','sheetWidth : ',this.sheetWidth,'sheetHeight : ',this.sheetHeight);
+		
+		this.sheetWinXPos = 0;
+		this.sheetWinYPos = 0;
+		//console.log('[Class Ball]\n','sheetWinXPos : ',this.sheetWinXPos,'sheetWinYPos : ',this.sheetWinYPos);
+		
+		this.sheetWinWidth = (this.sheetWidth / this.noOfCols);
+		this.sheetWinHeight = (this.sheetHeight / this.noOfRows);
+		//console.log('[Class Ball]\n','sheetWinWidth : ',this.sheetWinWidth,'sheetWinHeight : ',this.sheetWinHeight);
+		
+		this.targetXPos = targetXPos;
+		this.targetYPos = targetYPos - (this.sheetWinHeight / 2);
+		
+		this.width = this.sheetWidth / this.noOfCols;
+		this.height = this.sheetHeight / this.noOfRows;
+		//console.log('[Class Ball]\n','width : ',this.width,'height : ',this.height);
+		
+		/*this.xDir = 0;
+		this.yDir = 0;*/
+		this.speed = 3;
+		this.xMovement = (this.targetXPos - this.xPos);
+		this.yMovement = (this.targetYPos - this.yPos);
+		this.totalMovement = Math.abs(this.xMovement) + Math.abs(this.yMovement);
+		this.dx = (this.xMovement / this.totalMovement) * this.speed;
+		this.dy = (this.yMovement / this.totalMovement) * this.speed;
+		this.angle = Math.atan(this.yMovement / this.xMovement);
+		
+		
+		this.isFacingLeft = isFacingLeft;
+		
+		this.sheetCurrentRow = 0;
+		this.sheetCurrentCol = 0;
+		this.cc = 0;
+	}
+
+	animate(sheetCurrentRow,sheetCurrentCol) 
+	{
+		//console.log('[Class Ball]\n','Function : animate()');
+		
+		this.sheetWinXPos = sheetCurrentCol * this.sheetWinWidth;
+		this.sheetWinYPos = sheetCurrentRow * this.sheetWinHeight;
+
+		//console.log('sheetWinXPos : ',this.sheetWinXPos,'sheetWinYPos : ',this.sheetWinYPos);
+	}
+
+	move() 
+	{
+		//console.log('[Class Ball]\n','Function : move()');
+		
+		this.xPos = this.xPos + (this.dx * this.speed); 
+		this.yPos = this.yPos + (this.dy * this.speed); 
+
+		//console.log('XPos : ',this.xPos,'YPos : ',this.yPos);
+	}
+
+	display() 
+	{
+		//console.log('[Class Ball]\n','Function : display()');
+
+		ctx.drawImage(this.sheet,this.sheetWinXPos,this.sheetWinYPos,this.sheetWinWidth,this.sheetWinHeight,this.xPos,this.yPos,this.width,this.height);
+		/*
+		//console.log('sheet : ',this.sheet,'\nsheetWinXPos : ',this.sheetWinXPos,'\nsheetWinYPos : ',this.sheetWinYPos
+					,'\nsheetWinWidth : ',this.sheetWinWidth,'\nsheetWinHeight : ',this.sheetWinHeight
+					,'\nXPos : ',this.xPos,'\nYPos : ',this.yPos,'\nwidth : ',this.width,'\nheight : ',this.height);
+		*/
+		//debugger;
+	}
+
+	rotate(degrees)
+	{
+		console.log('[Class Ball]\n','Function : rotate(',degrees,')');
+		ctx.save();
+		//ctx.translate(player1.xPos,player1.yPos);
+		ctx.rotate(degrees);
+		this.display();
+		dot();
+		this.dot();
+		//debugger
+		ctx.restore();
+	}
+	dot() 
+	{	
+		ctx.fillStyle = 'pink';
+		ctx.fillRect(this.xPos + (this.width),this.yPos + (this.height / 2),3,3);
+
+	}
+
+
+	nextSpriteCol()
+	{
+		//console.log('[Class Ball]\n','Function : nextSpriteCol()');
+
+		this.sheetCurrentCol = (this.sheetCurrentCol + 1) % (this.noOfCols - 4);			//	Since we only need sprites (0,3) to (0,7)
+
+		//console.log('sheetCurrentCol : ',this.sheetCurrentCol);
+	}
+
+	updateAnimation()
+	{
+		//console.log('[Class Ball]\n','Function : updateAnimation()');
+
+		if(this.cc === 0)
+		{
+			this.nextSpriteCol();	
+		
+			/*
+			if (this.isCollision()) 
+			{
+				//console.log('[Class Ball]\n','DirChange : ',this.isDirChange());
+				this.sheetCurrentCol = 0;
+				//console.log('sheetCurrentCol : ',this.sheetCurrentCol);
+			}
+			*/
+
+			if (this.isFacingLeft) 
+			{
+				//console.log('[Class Ball]\n','FacingLeft : ',this.isFacingLeft);
+				this.sheetCurrentRow = 2;			//	Row 0 Mirror (i.e. Right to Left)
+				this.sheetCurrentCol = this.sheetCurrentCol + 4;
+				//console.log('sheetCurrentRow : ',this.sheetCurrentRow);
+				//console.log('sheetCurrentCol : ',this.sheetCurrentCol);
+			}
+			else
+			{
+				//console.log('[Class Ball]\n','FacingLeft : ',this.isFacingLeft);
+				this.sheetCurrentRow = 0;
+				//console.log('sheetCurrentRow : ',this.sheetCurrentRow);
+				//console.log('sheetCurrentCol : ',this.sheetCurrentCol);
+			}
+
+			this.animate(this.sheetCurrentRow,this.sheetCurrentCol); 
+		}
+
+		this.cc = (this.cc + 1) % 4; 
+		
+	}
+
+	
+
+	start()
+	{
+		//console.log('Function : start()');
+		
+		//clearCanvas();
+		this.move();
+		this.updateAnimation();
+		//this.rotate(this.angle);
+		this.display();
+		
+		//debugger
+	}
+}
 
 function animate() 
 {
 	//console.log('Function : animate()');
 		
-	clearCanvas();
-	//player1.updateAnimation();
+	//clearCanvas();
+	displayBg();
 	player1.display();	
+	for (var i = 0; i < player1.ammoArr.length; i++) 
+	{
+		player1.ammoArr[i].start();
+	}
 }
 
 function start() 
 {
 	//initPlayer1();
-	setInterval(animate,150);	
+	setInterval(animate,1000 / 60);	
 }
 
 start();
